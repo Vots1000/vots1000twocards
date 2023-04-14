d0b1d3a03f34159dc8875f4eea5389dd6aa54d98
Vots1000 <vots1000@gmail.com>
2023-04-14 21:35:15 +0200

    202304142135

--- src/core.js
+++ src/core.js
@@ -1,3 +1,11 @@
+const intervalId = setInterval(startTimer, 1000);
+const cardMinHeight = 55;
+const cardMinWidth = 36;
+const cardMaxHeight = 110;
+const cardMaxWidth = 72;
+var viewH = window.innerHeight;
+var viewV = window.innerWidth;
+
 function changeBg() {
     let bg = document.getElementById("bg").value;
     let urlBg = "src/bg/" + bg + ".jpg";
@@ -10,13 +18,13 @@ function changeStyle() {
     let cardsQ = cards.length;
     for (let i = 0; i < cardsQ; i++) {
         for (let i2 = 1; i2 <= 3; i2++) {
-         cards[i].classList.remove("card-style_" + i2);
+            cards[i].classList.remove("card-style_" + i2);
         }
         cards[i].classList.add("card-style_" + style);
     }
     let shown = document.getElementsByClassName("card-rotate_show180");
     let shownL = shown.length;
-    if(shownL >= 1) {
+    if ( shownL >= 1 ) {
         for (let i = 0 ; i < shownL ; i++) {
             let shownCard = shown[0];
             let shownCardId = shownCard.id;
@@ -24,26 +32,26 @@ function changeStyle() {
                 document.getElementById(shownCardId).classList.add("card-rotate_hide90");
                 hideCard(shownCardId);
         }
-    }    
+    }
 }
 
 function GameArea() {
     let cardsQuantity = document.getElementById("cards_quantity").value;
     changeBg();
-    if(cardsQuantity < 4 || cardsQuantity > 104 || cardsQuantity % 2 != 0) {
-        alert("Only even numbers from 2 to 104 are allowed.");
+    if ( cardsQuantity < 4 || cardsQuantity > 104 || cardsQuantity % 2 != 0 ) {
+        alert("Only even numbers from 4 to 104 are allowed.");
         exit;
-    }    
+    }
     document.getElementById("content_inner").innerHTML="";
     let style = document.getElementById("style").value;
     const cardsPairs = [];
-    for (let i = 1; i <=2; i++) {
-        for (let i2 = 1; i2 <= (cardsQuantity/2); i2++) {
+    for ( let i = 1; i <=2; i++ ) {
+        for ( let i2 = 1; i2 <= (cardsQuantity/2); i2++ ) {
             cardsPairs.push(i2);
         }
     }
     let Pairs = cardsPairs.sort(() => Math.random() - 0.5);
-    for (let i = 1 ; i <= cardsQuantity ; i++) {
+    for ( let i = 1 ; i <= cardsQuantity ; i++ ) {
         let box;
         let boxIn;
         box = document.createElement("div");
@@ -67,27 +75,28 @@ function GameArea() {
         box.appendChild(boxIn);
         document.getElementById("content_inner").appendChild(box);
     }
+    cardsScaling();
     document.getElementById("result").innerHTML="0";
     document.getElementById("play-time").innerHTML="0";
     document.getElementById("sec-per-pair").innerHTML="-";
 }
-function startTimer(){
+function startTimer() {
     let tt;
-    if(document.getElementsByClassName("card").length >= 1) {
-        let t = document.getElementById("play-time").innerHTML; 
+    if ( document.getElementsByClassName("card").length >= 1 ) {
+        let t = document.getElementById("play-time").innerHTML;
         tt = parseFloat(t) + 1;
-        document.getElementById("play-time").innerHTML = tt; 
+        document.getElementById("play-time").innerHTML = tt;
     } else {
         tt = document.getElementById("play-time").innerHTML;
     }
     let p = document.getElementById("result").innerHTML;
-    if(parseFloat(p) != 0) {
+    if ( parseFloat(p) != 0 ) {
         let spp = tt/parseFloat(p);
         document.getElementById("sec-per-pair").innerHTML = spp.toFixed(2);
     }
 }
 
-const intervalId = setInterval(startTimer, 1000);
+
 
 $(document).ready(function() {
     $("button").on("click", function() {
@@ -101,16 +110,104 @@ $(document).ready(function() {
     });
 });
 
+$(window).resize(function() {
+    cardsScaling();
+});
+
+function cardsScaling() {
+    if( !document.getElementById("end-box") ) {
+        let cardWidth = 72;
+        let cardHeight = 110;
+        let cardGetWidth = document.getElementsByClassName("card")[0].style.width;
+        let cardGetHeight = document.getElementsByClassName("card")[0].style.height;
+        if( !cardGetWidth || !cardGetHeight || cardGetWidth == '' || cardGetHeight == '' ) {
+            cardGetWidth = 72;
+            cardGetHeight = 110;
+            for ( let i = 0 ; i < document.getElementsByClassName("card").length ; i++ ) {
+                document.getElementsByClassName("card")[i].style.width = "72px";
+                document.getElementsByClassName("card")[i].style.height = "110px";
+            }
+            for ( let i = 0 ; i < document.getElementsByClassName("card-place").length ; i++ ) {
+                document.getElementsByClassName("card-place")[i].style.width = "72px";
+                document.getElementsByClassName("card-place")[i].style.height = "110px";
+            }
+        } else {
+            cardGetWidth = parseFloat(cardGetWidth.replace("px",""));
+            cardGetHeight = parseFloat(cardGetHeight.replace("px",""));
+        }
+        let resizeDone = false;
+        while ( resizeDone != true ) {
+            var contentHeight = document.body.scrollHeight;
+            var windowHeight = document.body.clientHeight;
+            if ( contentHeight <= windowHeight ) {
+                if( cardGetWidth == 72 || cardGetHeight == 110 ) {
+                    resizeDone = true;
+                } else if ( cardGetWidth > 72 || cardGetHeight > 110 ) {
+                    cardWidth = 72;
+                    cardHeight = 110;
+                    for ( let i = 0 ; i < document.getElementsByClassName("card").length ; i++ ) {
+                        document.getElementsByClassName("card")[i].style.width = (cardWidth + "px");
+                        document.getElementsByClassName("card")[i].style.height = (cardHeight + "px");
+                    }
+                    for ( let i = 0 ; i < document.getElementsByClassName("card-place").length ; i++ ) {
+                        document.getElementsByClassName("card-place")[i].style.width = (cardWidth + "px");
+                        document.getElementsByClassName("card-place")[i].style.height = (cardHeight + "px");
+                    }
+                    resizeDone = true;
+                } else if ( (cardGetWidth < 72 || cardGetHeight < 110) && (document.body.clientHeight > viewH || document.body.clientWidth > viewV) ) {
+                    cardWidth = 72;
+                    cardHeight = 110;
+                    for ( let i = 0 ; i < document.getElementsByClassName("card").length ; i++ ) {
+                        document.getElementsByClassName("card")[i].style.width = (cardWidth + "px");
+                        document.getElementsByClassName("card")[i].style.height = (cardHeight + "px");
+                    }
+                    for ( let i = 0 ; i < document.getElementsByClassName("card-place").length ; i++ ) {
+                        document.getElementsByClassName("card-place")[i].style.width = (cardWidth + "px");
+                        document.getElementsByClassName("card-place")[i].style.height = (cardHeight + "px");
+                    }
+                    resizeDone = true;
+                    cardsScaling();
+               } else {
+                   resizeDone = true;
+               }
+           } else if ( contentHeight > windowHeight ) {
+               if( cardGetWidth <= 36 || cardGetHeight <= 55 ) {
+                   cardWidth = 36;
+                   cardHeight = 55;
+                   resizeDone = true;
+               } else if ( cardWidth <= 36 || cardHeight <= 55 ) {
+                   cardWidth = 36;
+                   cardHeight = 55;
+                   resizeDone = true;
+               } else {
+                   cardWidth = cardWidth * 0.9;
+                   cardHeight = cardHeight * 0.9;
+               }
+               for (let i = 0 ; i < document.getElementsByClassName("card").length ; i++) {
+                   document.getElementsByClassName("card")[i].style.width = (cardWidth + "px");
+                   document.getElementsByClassName("card")[i].style.height = (cardHeight + "px");
+               }
+               for (let i = 0 ; i < document.getElementsByClassName("card-place").length ; i++) {
+                   document.getElementsByClassName("card-place")[i].style.width = (cardWidth + "px");
+                   document.getElementsByClassName("card-place")[i].style.height = (cardHeight + "px");
+               }
+            }
+        }
+    }
+    viewH = document.body.clientHeight;
+    viewV = document.body.clientWidth;
+}
+
 function cardClick() {
     let style = document.getElementById("style").value;
     let shown = document.getElementsByClassName("card-rotate_show180");
     let shownL = shown.length;
     let thisId = this.id;
     let shownTwo = false;
-    if(shownL == 2) {
+    if ( shownL == 2 ) {
         let shownCard = shown[0];
         shownTwo = true;
-        for (let i = 0 ; i < shownL ; i++) {
+        for ( let i = 0 ; i < shownL ; i++ ) {
             let shownCardId = shownCard.id;
             if ( shownCardId != thisId ) {
                 document.getElementById(shownCardId).classList.remove("card-rotate_show180");
@@ -122,17 +219,16 @@ function cardClick() {
             }
         }
     } else {
-    shownTwo = false;
+        shownTwo = false;
     }
-
     let getClass = this.className;
-    if(getClass.includes("card-rotate_show180") == true && shownTwo == false) {
+    if ( getClass.includes("card-rotate_show180") == true && shownTwo == false ) {
         this.classList.remove("card-rotate_show180");
         this.classList.add("card-rotate_hide90");
         hideCard(this.id);
-    } else if (getClass.includes("card-rotate_show180") == true && shownTwo == true) {
-    
-    } else if (getClass.includes("card-rotate_hide180") == true) {
+    } else if ( getClass.includes("card-rotate_show180") == true && shownTwo == true ) {
+
+    } else if ( getClass.includes("card-rotate_hide180") == true ) {
         this.classList.remove("card-rotate_hide180");
         this.classList.add("card-rotate_show90");
         showCard(this.id);
@@ -141,18 +237,18 @@ function cardClick() {
         showCard(this.id);
     }
 }
-    
+
 function showCard(cardId) {
     document.getElementById(cardId).addEventListener("animationend", (event) => {
- 
-        if(document.getElementById(cardId).className.includes("card-rotate_show90")) {
+        if ( document.getElementById(cardId).className.includes("card-rotate_show90") ) {
 
-            if(document.getElementById(cardId).className.includes("card-style_3")) {
+            if ( document.getElementById(cardId).className.includes("card-style_3") ) {
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.color= "#000000";
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.fontSize= "0px";
             document.getElementById(cardId).getElementsByClassName("card-inner")[0].style.backgroundColor = ("#ffffff");
             let cardno = document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].innerText;
             document.getElementById(cardId).getElementsByClassName("card-inner")[0].style.backgroundImage = ("url('src/cards/3/" + cardno +".jpg')");
+            document.getElementById(cardId).getElementsByClassName("card-inner")[0].style.backgroundSize = ("100%");
             } else {
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.color= "#000000";
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.fontSize= "40px";
@@ -165,9 +261,9 @@ function showCard(cardId) {
     });
 }
 
-function hideCard(cardId) {
+function hideCard( cardId ) {
     document.getElementById(cardId).addEventListener("animationend", (event) => {
- 
+
         if(document.getElementById(cardId).className.includes("card-rotate_hide90")) {
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.color= "transparent";
             document.getElementById(cardId).getElementsByClassName("card-inner-span")[0].style.fontSize= "0px";
@@ -181,12 +277,12 @@ function hideCard(cardId) {
 }
 
 
-function checkPairs(cardId) {
+function checkPairs( cardId ) {
     let shown2 = document.getElementsByClassName("card-rotate_show180");
     let shownL2 = shown2.length;
 
-    if(shownL2 == 2) {
-        if (shown2[0].getElementsByClassName("card-inner-span")[0].innerHTML == shown2[1].getElementsByClassName("card-inner-span")[0].innerHTML) {
+    if ( shownL2 == 2 ) {
+        if ( shown2[0].getElementsByClassName("card-inner-span")[0].innerHTML == shown2[1].getElementsByClassName("card-inner-span")[0].innerHTML ) {
         let divId1 = shown2[0].id;
         let divId2 = shown2[1].id;
             document.getElementById(cardId).addEventListener("animationend", (event) => {
@@ -194,7 +290,7 @@ function checkPairs(cardId) {
             document.getElementById(divId1).className = ("card-place");
             document.getElementById(divId2).style.visibility = "hidden";
             document.getElementById(divId2).className = ("card-place");
-            if(document.getElementsByClassName("card").length == 0){
+            if ( document.getElementsByClassName("card").length == 0 ) {
                gameEnd();
             }
         });
